12ccf4b36ea91db1a33ebf859a134e226a97d569
David Leon Ortega <david_lagu@hotmail.com>
2017-03-06 13:23:44 +0000

    mejoras interfaz gráfica

--- Comunicación Socket/cliente.py
+++ Comunicación Socket/cliente.py
@@ -6,22 +6,26 @@ except ImportError:
 	
 
 root = Tk()
+
+#conection Frame
 window = Frame(root)
+Label(window,text="Datos de conexión")
 window.pack()
+
 Label(window,text="IP").grid(row=0)
 ipCon = Entry(window)
 ipCon.grid(row=0,column=1)
 conButton = Button(window,text="Conectar").grid(row=1,column=0,padx=5,pady=10)
 endConButton = Button(window,text="Cerrar conexión").grid(row=1,column=3,padx=5,pady=10)
 
+#Data Box
 text = Frame(root)
 text.pack()
 inText = Text(text,height=4).pack()
-#frame = Frame(window)
-#frame.pack(fill=BOTH)
-#conButton = Button(frame,text="Conectar")
-#endConButton = Button(frame,text="Cerrar conexión")
 
-#conButton.pack(side = LEFT)
-#endConButton.pack(side = RIGHT)
+
+send = Frame(root)
+send.pack()
+sendButton = Button(send,text="Enviar").grid(row=1,column=0,padx=5,pady=10)
+clearConButton = Button(send,text="Limpiar").grid(row=1,column=3,padx=5,pady=10)
 window.mainloop()
