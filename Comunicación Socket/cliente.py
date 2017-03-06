32a47521c7cd322f24d3d5482658d3ed9754bb42
David Leon Ortega <david_lagu@hotmail.com>
2017-03-06 11:19:30 +0000

    Interfaz gráfica
    Añadidos Botones:
    -Realizar Conexión.
    - Cerrar Conexión.
    Campo de entrada de IP a la que conectar.

--- Comunicación Socket/cliente.py
+++ Comunicación Socket/cliente.py
@@ -1,1 +1,22 @@
-from RKinter import*
+#!/usr/bin/python3
+try:
+	from Tkinter import*
+except ImportError:
+	from tkinter import*
+	
+
+window = Tk()
+Label(window,text="IP").grid(row=0)
+ipCon = Entry(window)
+ipCon.grid(row=0,column=1)
+conButton = Button(window,text="Conectar").grid(row=1,column=0,padx=5,pady=10)
+endConButton = Button(window,text="Cerrar conexión").grid(row=1,column=3,padx=5,pady=10)
+
+#frame = Frame(window)
+#frame.pack(fill=BOTH)
+#conButton = Button(frame,text="Conectar")
+#endConButton = Button(frame,text="Cerrar conexión")
+
+#conButton.pack(side = LEFT)
+#endConButton.pack(side = RIGHT)
+window.mainloop()
