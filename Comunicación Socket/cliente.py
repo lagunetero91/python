5c2f6718b97edab694afa91c75c019496d7e3628
David Leon Ortega <david_lagu@hotmail.com>
2017-03-06 11:45:16 +0000

    Añadido Cuadro paa escribir texto

--- Comunicación Socket/cliente.py
+++ Comunicación Socket/cliente.py
@@ -5,13 +5,18 @@ except ImportError:
 	from tkinter import*
 	
 
-window = Tk()
+root = Tk()
+window = Frame(root)
+window.pack()
 Label(window,text="IP").grid(row=0)
 ipCon = Entry(window)
 ipCon.grid(row=0,column=1)
 conButton = Button(window,text="Conectar").grid(row=1,column=0,padx=5,pady=10)
 endConButton = Button(window,text="Cerrar conexión").grid(row=1,column=3,padx=5,pady=10)
 
+text = Frame(root)
+text.pack()
+inText = Text(text,height=4).pack()
 #frame = Frame(window)
 #frame.pack(fill=BOTH)
 #conButton = Button(frame,text="Conectar")
