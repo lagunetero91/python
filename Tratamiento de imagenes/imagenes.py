7fd38b57eb6beb289a56bb9979460944d73cb8f9
David Leon Ortega <david_lagu@hotmail.com>
2017-03-16 18:54:04 +0000

    Frame

--- Tratamiento de imagenes/imagenes.py
+++ Tratamiento de imagenes/imagenes.py
@@ -3,9 +3,17 @@ import tkinter
 from PIL import Image, ImageTk
 
 size = 256, 256
-image = Image.open("img.jpg")				#Abrir Imagen
+
 root = tkinter.Tk()
+
+window = tkinter.Frame(root)
+window.pack()
+image = Image.open("img.jpg")				#Abrir Imagen
 image.thumbnail(size, Image.ANTIALIAS)		#Cambia el tamaño de la imagen
 tkimage = ImageTk.PhotoImage(image)			#Mostrar imagen
-tkinter.Label(root, image=tkimage).pack()
+tkinter.Label(window, image=tkimage).pack()
+
+
+
+
 root.mainloop()
