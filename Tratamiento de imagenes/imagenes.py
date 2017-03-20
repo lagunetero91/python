91edfe59bfd0ae90a04fddd8ee47a540490a169f
David Leon Ortega <david_lagu@hotmail.com>
2017-03-20 17:51:35 +0000

    Añadido filtro "Escala de grises"

--- Tratamiento de imagenes/imagenes.py
+++ Tratamiento de imagenes/imagenes.py
@@ -4,14 +4,15 @@ import tkinter.filedialog
 
 from PIL import Image, ImageTk,ImageFilter,ImageOps
 
-window_size= "640x480"
-size = 256, 256
+window_size= "640x480"      #Tamaño de la ventana del programa.
+size = 256, 256             #Tamaño de las miniaturas de las imágenes.
 
-filters = {'Invertir color', 'Normal'}
+filters = {'Invertir color', 'Normal','Escala de grises'}      #ComboBox: Filtros
 root = tkinter.Tk()
 tkvar = StringVar(root)
 tkvar.set('Invertir color')
 
+# Método que se utiliza para cargar una imagen al programa.
 def chooseImage():
     global acI
     filename =tkinter.filedialog.askopenfilename()
@@ -23,19 +24,19 @@ def chooseImage():
     panel.image = tkimage2
     acI = inImage2
    
-
+#Método encargado de guardar la imagen procesada.
 def saveImage():
     global outI
     savefile = tkinter.filedialog.asksaveasfile(mode='w',defaultextension=".jpg")
     if savefile:    #Comprueba si se le dío a cancelar.
         outI.save(savefile)
 
+#Método encargado de aplicar los filtros.
 def aplyFilter():
     global acI
     global outI
     auxiliarImg = acI
     filter = tkvar.get()
-    print(filter)
     if filter == 'Invertir color' :
         showIm = ImageOps.invert(auxiliarImg)
         outI=showIm
@@ -46,25 +47,33 @@ def aplyFilter():
         tkimageout2 = ImageTk.PhotoImage(acI)			#Mostrar imagen
         panel2.configure(image = tkimageout2)
         panel2.image = tkimageout2
+    elif filter == 'Escala de grises':
+        showIm = acI.convert("L")
+        outI=showIm
+        tkimageout2 = ImageTk.PhotoImage(showIm)			#Mostrar imagen
+        panel2.configure(image = tkimageout2)
+        panel2.image = tkimageout2
 
 root.geometry(window_size)
 window = tkinter.Frame(root)
 window.pack()
-inImage = Image.open("intro.jpg")#Abrir Imagen
+inImage = Image.open("intro.jpg")                       #Abrir Imagen por defecto de la entrada.
 acI = inImage
 outI = inImage
 inImage.thumbnail(size, Image.ANTIALIAS)		#Cambia el tamaño de la imagen
 tkimage = ImageTk.PhotoImage(inImage)			#Mostrar imagen
 panel = tkinter.Label(window, image=tkimage)
 panel.grid(row=0)
-outputimage = Image.open("result.jpg")				#Abrir Imagen
+
+outputimage = Image.open("result.jpg")			#Abrir Imagen por defecto de la salida.
 outputimage.thumbnail(size, Image.ANTIALIAS)		#Cambia el tamaño de la imagen
 tkimageout = ImageTk.PhotoImage(outputimage)
 panel2 = tkinter.Label(window, image=tkimageout)
 panel2.grid(row=0,column=2)
-chooseButton = tkinter.Button(window,text="Selecionar Imagen",command=chooseImage).grid(row=1,column=0,pady=8)
-saveButton = tkinter.Button(window,text="Guadar Imagen",command=saveImage).grid(row=1,column=2)
+
+chooseButton = tkinter.Button(window,text="Selecionar Imagen",command=chooseImage).grid(row=1,column=0,pady=8)      #Botón de carga de imágenes
+saveButton = tkinter.Button(window,text="Guadar Imagen",command=saveImage).grid(row=1,column=2)                     #Botón para guardar imágenes
 Label(window,text="Seleccione filtro: ").grid(row=2,column=0)
-filterMenu = OptionMenu(window,tkvar,*filters).grid(row=2,column=1)
-filerButton = tkinter.Button(window,text="Aplicar Filtro",command=aplyFilter).grid(row=2,column=2,pady= 30)
+filterMenu = OptionMenu(window,tkvar,*filters).grid(row=2,column=1)                                                 #ComboBox
+filerButton = tkinter.Button(window,text="Aplicar Filtro",command=aplyFilter).grid(row=2,column=2,pady= 30)         #Botón que aplica el filtro seleccionado por el ComboBox
 root.mainloop()
