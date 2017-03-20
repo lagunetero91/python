bc4cbbb13791f71d577980539484e06b847fd95b
David Leon Ortega <david_lagu@hotmail.com>
2017-03-20 15:47:15 +0000

    Añadido primer filtro

--- Tratamiento de imagenes/imagenes.py
+++ Tratamiento de imagenes/imagenes.py
@@ -1,36 +1,55 @@
 import os, sys
 from tkinter import*
 import tkinter.filedialog
-from PIL import Image, ImageTk
+
+from PIL import Image, ImageTk,ImageFilter,ImageOps
 
 window_size= "640x480"
 size = 256, 256
 root = tkinter.Tk()
 
+
 def chooseImage():
+    global acI
     filename =tkinter.filedialog.askopenfilename()
     print(filename)
-    image2 = Image.open(filename)				#Abrir Imagen
-    image2.thumbnail(size, Image.ANTIALIAS)		#Cambia el tamaño de la imagen
-    tkimage2 = ImageTk.PhotoImage(image2)			#Mostrar imagen
+    inImage2 = Image.open(filename)				#Abrir Imagen
+    inImage2.thumbnail(size, Image.ANTIALIAS)		#Cambia el tamaño de la imagen
+    tkimage2 = ImageTk.PhotoImage(inImage2)			#Mostrar imagen
     panel.configure(image = tkimage2)
     panel.image = tkimage2
+    acI = inImage2
+   
 
 def saveImage():
     savefile = tkinter.filedialog.asksaveasfile(mode='w',defaultextension=".jpg")
     if savefile:    #Comprueba si se le dío a cancelar.
         image.save(savefile)
+
+def aplyFilter():
+    global acI
+    auxiliarImg = acI
+    showIm = ImageOps.invert(auxiliarImg)
     
+    tkimageout2 = ImageTk.PhotoImage(showIm)			#Mostrar imagen
+    panel2.configure(image = tkimageout2)
+    panel2.image = tkimageout2
+
 root.geometry(window_size)
 window = tkinter.Frame(root)
 window.pack()
-image = Image.open("intro.jpg")				#Abrir Imagen
-image.thumbnail(size, Image.ANTIALIAS)		#Cambia el tamaño de la imagen
-tkimage = ImageTk.PhotoImage(image)			#Mostrar imagen
+inImage = Image.open("intro.jpg")#Abrir Imagen
+acI = inImage
+inImage.thumbnail(size, Image.ANTIALIAS)		#Cambia el tamaño de la imagen
+tkimage = ImageTk.PhotoImage(inImage)			#Mostrar imagen
 panel = tkinter.Label(window, image=tkimage)
 panel.grid(row=0)
-panel2 = tkinter.Label(window, image=tkimage)
+outputimage = Image.open("result.jpg")				#Abrir Imagen
+outputimage.thumbnail(size, Image.ANTIALIAS)		#Cambia el tamaño de la imagen
+tkimageout = ImageTk.PhotoImage(outputimage)
+panel2 = tkinter.Label(window, image=tkimageout)
 panel2.grid(row=0,column=1)
 chooseButton = tkinter.Button(window,text="Selecionar Imagen",command=chooseImage).grid(row=1,column=0)
 saveButton = tkinter.Button(window,text="Guadar Imagen",command=saveImage).grid(row=1,column=1)
+filerButton = tkinter.Button(window,text="Aplicar Filtro",command=aplyFilter).grid(row=2,column=0)
 root.mainloop()
