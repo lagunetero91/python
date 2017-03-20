14bcbbd5051bcbbba13242f841ce07279a8e7ee6
David Leon Ortega <david_lagu@hotmail.com>
2017-03-20 14:57:34 +0000

    Guardar archivo

--- Tratamiento de imagenes/imagenes.py
+++ Tratamiento de imagenes/imagenes.py
@@ -1,5 +1,5 @@
 import os, sys
-import tkinter
+from tkinter import*
 import tkinter.filedialog
 from PIL import Image, ImageTk
 
@@ -16,7 +16,11 @@ def chooseImage():
     panel.configure(image = tkimage2)
     panel.image = tkimage2
 
-
+def saveImage():
+    savefile = tkinter.filedialog.asksaveasfile(mode='w',defaultextension=".jpg")
+    if savefile:    #Comprueba si se le dío a cancelar.
+        image.save(savefile)
+    
 root.geometry(window_size)
 window = tkinter.Frame(root)
 window.pack()
@@ -24,6 +28,9 @@ image = Image.open("intro.jpg")				#Abrir Imagen
 image.thumbnail(size, Image.ANTIALIAS)		#Cambia el tamaño de la imagen
 tkimage = ImageTk.PhotoImage(image)			#Mostrar imagen
 panel = tkinter.Label(window, image=tkimage)
-panel.pack()
-chooseButton = tkinter.Button(window,text="Selecionar Imagen",command=chooseImage).pack()
+panel.grid(row=0)
+panel2 = tkinter.Label(window, image=tkimage)
+panel2.grid(row=0,column=1)
+chooseButton = tkinter.Button(window,text="Selecionar Imagen",command=chooseImage).grid(row=1,column=0)
+saveButton = tkinter.Button(window,text="Guadar Imagen",command=saveImage).grid(row=1,column=1)
 root.mainloop()
